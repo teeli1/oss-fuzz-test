d4f9f0c78d0413c4a3b6e2f9f787292a46cfa338
teeli1 <20211065@sungshin.ac.kr>
2024-09-24 17:16:04 +0900

    build.sh

--- oss-fuzz-test/build.sh
+++ oss-fuzz-test/build.sh
@@ -1,3 +1,4 @@
-$CC $CFLAGS work1.c -c -o work1.o
-$CC $CFLAGS fuzz_work1.c -c -o fuzz_work1.o
-$CC $CFLAGS $LIB_FUZZING_ENGINE fuzz_work1.o work1.o -o $OUT/simple-fuzzer
+$CC $CFLAGS -o work1.o work1.c
+$CC $CFLAGS -o fuzz_work1.o fuzz_work1.c
+$CC $CFLAGS $LIB_FUZZING_ENGINE fuzz_work1.o work1.o -o $OUT/oss-fuzz-test
+
